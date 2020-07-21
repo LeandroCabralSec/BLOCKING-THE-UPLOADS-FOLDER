bd6b7b92baaf507cf663f23cc37ff46d8f753341
Leandro Cabral <68610108+LeandroCabralSec@users.noreply.github.com>
2020-07-21 20:32:24 -0300

    Update wp-content.php

--- wp-content.php
+++ wp-content.php
@@ -1,3 +1,9 @@
+A pasta de uploads é o local em que todas as suas imagens e outras mídias são armazenadas. No entanto, por ser gravável, também pode ser usados por hackers, 
+que podem usá-lo para fazer upload e executar malware.
+
+The uploads folder is the place where all your images and other media are stored. However, because it is writable, it can also be used by hackers, who can 
+use it to upload and run malware.
+
 Acesse a pasta wp-content.php e depois a pasta Uploads, crie um arquivo .htaccess e coloque o código abaixo.
 
 Access the wp-content.php folder and then the Uploads folder, create a .htaccess file and place the code below.
